fa759d5dae3d9a7e00c92cd3a89152b579ddc760
Asad Hameed <asadhameed211@gmail.com>
2021-05-31 12:55:43 +0500

    Initial setup

---
+++ lib/salesloft.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'httparty'
+
+module Salesloft
+  class << self
+    def get_people
+      response = HTTParty.get("#{ENV['SALESLOFT_API_URL']}/#{ENV['SALESLOFT_API_VERSION']}/#{SALESLOFT_GET_PEOPLE_PATH}",
+                              headers: default_headers)
+                          .parsed_response
+
+      response
+    end
+
+    private
+
+    SALESLOFT_GET_PEOPLE_PATH = 'people'
+
+    private_constant :SALESLOFT_GET_PEOPLE_PATH
+
+    def default_headers
+      {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer #{ENV['SALESLOFT_API_TOKEN']}"
+      }
+    end
+  end
+end
